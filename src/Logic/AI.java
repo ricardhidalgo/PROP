58808ba51deffc802c45e3dadb37b715568868ec
Ricard <ricard.hidalgo.95@gmail.com>
2017-11-13 23:56:23 +0100

    AI inicis

---
+++ src/Logic/AI.java
@@ -0,0 +1,23 @@
+package Logic;
+
+public class AI {
+
+
+
+    public Combination generateSecret(Difficulty dif){
+
+        return null;
+
+    }
+    public Combination generateFirstCombination() {
+
+        return null;
+
+    }
+    public Combination generateNextCombination(Play result) {
+
+        return null;
+
+    }
+
+}
---
+++ src/Logic/AI_Genetic.java
@@ -0,0 +1,15 @@
+package Logic;
+
+public class AI_Genetic extends AI {
+
+
+    public Combination generateSecret(Difficulty dif){
+
+    }
+    public Combination generateFirstCombination() {
+
+    }
+    public Combination generateNextCombination(Play result) {
+
+    }
+}
