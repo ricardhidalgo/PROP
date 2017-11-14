78b17d7cf22946f748ba9858e4545f3d39746b1f
Ricard <ricard.hidalgo.95@gmail.com>
2017-11-14 01:04:34 +0100

    AI continuacio

--- src/Logic/AI_Genetic.java
+++ src/Logic/AI_Genetic.java
@@ -1,9 +1,30 @@
 package Logic;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class AI_Genetic extends AI {
 
 
     public Combination generateSecret(Difficulty dif){
+        Random random = new Random();
+
+        if(dif.isCanRepeat()) {
+            ArrayList<Integer> comb = new ArrayList<>();
+            for (int i = 0; i < dif.getNumBallsInCombination(); ++i) {
+                comb.add(random.nextInt(8));
+            }
+
+        } else {
+
+            Set<Integer> hcomb = new HashSet<>();
+            while(hcomb.size() < dif.getNumBallsInCombination()) {
+                hcomb.add(random.nextInt(8));
+            }
+
+        }
 
     }
     public Combination generateFirstCombination() {
