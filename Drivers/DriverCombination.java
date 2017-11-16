4cd166410a9d5ca7ceec467047c959c3b05c6924
Ricard <ricard.hidalgo.95@gmail.com>
2017-11-16 23:56:12 +0100

    Driver Combination

--- Drivers/DriverCombination.java
+++ Drivers/DriverCombination.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import Logic.Combination;
+
+import java.util.Scanner;
+
 
 public class DriverCombination {
+
+    public static void main(String[] args) {
+        Combination cont = new Combination();
+        Scanner sc = new Scanner(System.in);
+        try {
+            while (true) {
+                System.out.println();
+                System.out.println("Indique qué desea hacer:");
+                System.out.println("1. Instanciar contenido vacío");
+                System.out.println("2. Instanciar contenido con un String");
+                System.out.println("3. Get contenidoOriginal");
+                System.out.println("4. Set contenidoOriginal con un String");
+                System.out.println("5. Get contenidoReducido");
+                System.out.println("6. Calcular distancia respecto a otro contenido");
+                System.out.println("7. Salir");
+                int op = sc.nextInt();
+
+
+            }
+        }
+
+
+
+
+    }
+
 }
