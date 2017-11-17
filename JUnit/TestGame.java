46fd1eec162ef777f35bfea7b4a7af86520a9313
polgil <polgilhernandez@gmail.com>
2017-11-17 11:52:17 +0100

    JUNIT

--- JUnit/TestGame.java
+++ JUnit/TestGame.java
@@ -17,27 +17,22 @@ public class TestGame {
         Combination comb = new Combination(combi);
         ArrayList<Play> plays = new ArrayList<Play>();
         Difficulty difficulty = new Difficulty();
+        AI_Genetic ia = new AI_Genetic(difficulty);
         ArrayList<Byte> combi2 = new ArrayList<Byte>();
         combi.add((byte)2);
         Combination comb2 = new Combination(combi2);
-        Game play = new Game(aux, true, comb, plays, difficulty);
+        Game play = new Game(aux, ia, true, comb, plays, difficulty);
         play.setSecretCode(comb2);
         assertEquals(comb2, play.getSecretCode());
     }
 
     @Test
-    public void makePlay() {
-        private ArrayList<Byte> correctColors;
-        private Combination combination;
-        private int numCorrectColors;
-        private int numCorrectPositions;
-
-
-
-
-
-
-
+    public void getUser() {
+        User usuario = new User("Pol","45");
+        Game play = new Game();
+        User rta = new User();
+        rta = play.getUser();
+        assertEquals(usuario, rta);
     }
 
 
