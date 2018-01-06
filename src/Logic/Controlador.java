bc042ad8ca14f009fa325d4b44327e5f87203c21
polgil <polgilhernandez@gmail.com>
2018-01-06 22:01:40 +0100

    Acabando controladores

--- src/Logic/Controlador.java
+++ src/Logic/Controlador.java
@@ -3,7 +3,23 @@ package Logic;
 public class Controlador {
 
     private void iniciar() {
-       miCoordinador =
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        VentanaRegistro miVentanaRegistro = new VentanaRegistro();
+        VentanaDificultad miVentanaDificultad = new VentanaDificultad();
+        VentanaPartida miVentanaPartida = new VentanaPartida();
+        VentanaFinPartida miVentanaFinPartida = new VentanaFinPartida();
+        VentanaGuardar miVentanaGuardar = new VentanaGuardar();
 
     }
+
+    public static void main(String args[]) {
+        iniciar();
+        if (miVentanaPrincipal.modo == "registrar") User jugador = new User(miVentanaRegistro.nickname, miVentanaRegistro.password),
+        else if (miVentanaPrincipal.modo == "identificar") {
+            if (dg.validateUser(miVentanaRegistro.nickname))
+
+        }
+    }
+
+
 }
---
+++ src/Logic/main.java
@@ -0,0 +1,11 @@
+package Logic;
+
+public class main {
+
+    public static void main(String args[]) {
+        public void start() {
+            ControladorPresentacion mainWindow = new ControladorPresentacion();
+            mainWindow.iniciar();
+        }
+    }
+}
