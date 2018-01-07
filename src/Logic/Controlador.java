ef19fbfb8745c039145fb1da20f21395fbfddf5c
polgil <polgilhernandez@gmail.com>
2018-01-07 21:26:21 +0100

    Controlador = ControladorPresentacion

--- src/Logic/Controlador.java
+++ src/Logic/Controlador.java
@@ -1,25 +1,22 @@
 package Logic;
 
+import Presentation.Screen;
+
+import java.awt.*;
+
 public class Controlador {
 
-    /*private void iniciar() {
-        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
-        VentanaRegistro miVentanaRegistro = new VentanaRegistro();
-        VentanaDificultad miVentanaDificultad = new VentanaDificultad();
-        VentanaPartida miVentanaPartida = new VentanaPartida();
-        VentanaFinPartida miVentanaFinPartida = new VentanaFinPartida();
-        VentanaGuardar miVentanaGuardar = new VentanaGuardar();
+    private static void iniciar() {
+        Toolkit t = Toolkit.getDefaultToolkit();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Screen miVentanaPrincipal = new Screen(screenSize.width, screenSize.height); // Autoajusta la pantalla
+
 
     }
 
     public static void main(String args[]) {
         iniciar();
-        if (miVentanaPrincipal.modo == "registrar") User jugador = new User(miVentanaRegistro.nickname, miVentanaRegistro.password),
-        else if (miVentanaPrincipal.modo == "identificar") {
-            if (dg.validateUser(miVentanaRegistro.nickname))
-
-        }
-    }*/
+    }
 
 
 }
